04c4a6e74f4c42c2cbb8b930e11b36c72ead5e22
osuarez <omsulo@gmail.com>
2025-06-18 23:41:55 +0200

    changes to milestone file

--- mem_alloc/lesson1/milestone.c
+++ mem_alloc/lesson1/milestone.c
@@ -15,6 +15,7 @@ typedef struct block_t {
 
 
 Block_t* mem_chunk(size_t size) {
+    // this gives us 4046 bytes of raw memory
     Block_t* memory = mmap(
         NULL,
         size + sizeof(Block_t),
@@ -27,6 +28,9 @@ Block_t* mem_chunk(size_t size) {
         perror("mmap failed");
         return NULL;
     }
+
+    // the header of the block is at the beginning
+    // because we said so when organizing the struct
     Block_t* block = (Block_t*)memory;
     block->size = size;
     block->free = FREE;
@@ -35,6 +39,8 @@ Block_t* mem_chunk(size_t size) {
     return block;
 }
 
+// maintain a linked list of nodes with the info of all the 
+// used blocks
 Block_t* insert_at_the_end(Block_t* head, Block_t* new_chunk) {
     Block_t* current = head;
     while (current->next != NULL) {
@@ -44,15 +50,22 @@ Block_t* insert_at_the_end(Block_t* head, Block_t* new_chunk) {
     return head;
 }
 
+// get pointer to usable memory from a block
+void* get_usable_memory(Block_t* block) {
+    return (void*)(block + 1);
+}
+
 void iterate_over_all(Block_t* head) {
     Block_t* current = head;
 
     int count = 0;
     while (current != NULL) {
+        void* usable = get_usable_memory(current);
         printf("Node %d\n", count);
-        printf("Address: %p\n", current);
-        printf("Size: %ld\n", current->size);
-        printf("Total size for header + memory: %ld\n", current->size + sizeof(Block_t));
+        printf("Block header address: %p\n", (void*)current);
+        printf("Usable memory address: %p\n", usable);
+        printf("Size: %zu\n", current->size);
+        printf("Real size of the usable memory: %zu\n", current->size - sizeof(Block_t));
         printf("Is it free?: %d\n", current->free);
         current = current->next;
         count++;
