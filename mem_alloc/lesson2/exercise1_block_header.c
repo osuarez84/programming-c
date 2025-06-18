0048dd9799cf5fa8d750855a14cef29b519482c0
osuarez <omsulo@gmail.com>
2025-06-19 00:52:02 +0200

    added code for ex1

--- mem_alloc/lesson2/exercise1_block_header.c
+++ mem_alloc/lesson2/exercise1_block_header.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/mman.h>
 
 /*
  * Exercise 1: Design a Block Header Structure
@@ -19,6 +20,9 @@ typedef struct block_header {
     // - A way to link blocks together (for a free list)
     
     // Your implementation here...
+    size_t size;
+    bool is_free;
+    struct block_header* next;
     
 } block_header_t;
 
@@ -26,6 +30,10 @@ typedef struct block_header {
 void init_block_header(block_header_t* header, size_t size, bool is_free) {
     // TODO: Initialize the header fields
     // Your implementation here...
+    block_header_t* block = (block_header_t*)header;
+    block->size = size;
+    block->is_free = is_free;
+    block->next = NULL;
 }
 
 // Function to print block header information
@@ -34,6 +42,9 @@ void print_block_header(block_header_t* header) {
     // Your implementation here...
     printf("Block Header at %p:\n", (void*)header);
     // Print other fields...
+    printf("Size: %zu\n", header->size);
+    printf("Is it free?: %d\n", header->is_free);
+    printf("Next free block at address %p\n", header->next);
 }
 
 // This function demonstrates how the block header would be used
@@ -44,10 +55,22 @@ void demonstrate_block_header_usage() {
     
     // TODO: Create and initialize a sample block header
     // Your implementation here...
-    
+    size_t size = 4096;
+    block_header_t* mem_block = mmap(
+        NULL,
+        size,
+        PROT_READ | PROT_WRITE,
+        MAP_ANONYMOUS | MAP_PRIVATE,
+        -1,
+        0
+    );
+    init_block_header(mem_block, size, true);
+
     // TODO: Print the block header information
     // Your implementation here...
+    print_block_header(mem_block);
     
+
     printf("\nThis is how your memory allocator will use block headers:\n");
     printf("1. When allocating memory, it will search for a free block\n");
     printf("2. When it finds a suitable block, it will mark it as 'in use'\n");
